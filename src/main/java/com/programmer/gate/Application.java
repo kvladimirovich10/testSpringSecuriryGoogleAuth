6b8f5ea9cfd2eb7aee98697d41380079207b600a
ladimirovich <kvladimirovich10@gmail.com>
2018-11-06 20:19:36 +0300

    Initial commit

---
+++ src/main/java/com/programmer/gate/Application.java
@@ -0,0 +1,22 @@
+package com.programmer.gate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping(value = "/user")
+    public Principal user(Principal principal){
+        return principal;
+    }
+}
